fc8ef78183fde700b7279a63eec365a3a958e67d
zawadidev <cochsam3@gmail.com>
2026-03-31 16:13:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import os
 app = Flask(__name__)
 
 # Get keys from environment (set these on Render)
-CONSUMER_KEY = os.getenv("CONSUMER_KEY")
-CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+CONSUMER_KEY = os.getenv("jAJ7Ud37RnflT0xAovWjaTwlKytCQ6r0F77bdCfNk5dYhkqI")
+CONSUMER_SECRET = os.getenv("CUIchCcAb1EyqKGHLYhXJHCrsjDGml4qvVMr7d9uq33XKIfuS31YzYqILloKcJDQ")
 
 # Function to generate M-Pesa access token
 def get_access_token():
